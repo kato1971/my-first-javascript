822ca0a4daae6d7feddb8d1ec7e563ab0952e646
kato1971 <88402289+kato1971@users.noreply.github.com>
2021-12-22 12:45:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello Winc Academy");
+let name = "Kato";
+console.log(name);
+//let if = "Kato";
+// can not be a reserved keyword
+let number = 4 + 4;
+let numberS = "4" + "4";
+console.log(number, numberS);
+let y = 10;
+let x = y + 3;
+let a = y - 5;
+let b = y * 2;
+let c = b / 4;
+let d = a % 2;
+console.log(x, a, b, c, d);
+let age = 50;
+console.log (typeof age);
+let ageS = "fifty"
+console.log (typeof ageS)
